bae39c61aa09a8cb0bd0929b77be3e0e59e61907
Roman Rozhkov <enigma1800@gmail.com>
2019-07-14 01:58:15 +0300

    new project

---
+++ CityBO/OrderSearchForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CityBO
+{
+    partial class OrderSearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.OrderTextBoxOSF = new System.Windows.Forms.TextBox();
+            this.OrderSearchBut = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.OrderSearchBut);
+            this.groupBox1.Controls.Add(this.OrderTextBoxOSF);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(490, 54);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // OrderTextBoxOSF
+            // 
+            this.OrderTextBoxOSF.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OrderTextBoxOSF.Location = new System.Drawing.Point(7, 20);
+            this.OrderTextBoxOSF.Name = "OrderTextBoxOSF";
+            this.OrderTextBoxOSF.Size = new System.Drawing.Size(396, 20);
+            this.OrderTextBoxOSF.TabIndex = 0;
+            // 
+            // OrderSearchBut
+            // 
+            this.OrderSearchBut.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.OrderSearchBut.Location = new System.Drawing.Point(409, 18);
+            this.OrderSearchBut.Name = "OrderSearchBut";
+            this.OrderSearchBut.Size = new System.Drawing.Size(75, 23);
+            this.OrderSearchBut.TabIndex = 1;
+            this.OrderSearchBut.Text = "Поиск";
+            this.OrderSearchBut.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 73);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(490, 289);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Фильтры";
+            // 
+            // OrderSearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 374);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "OrderSearchForm";
+            this.Text = "OrderSearchForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button OrderSearchBut;
+        private System.Windows.Forms.TextBox OrderTextBoxOSF;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
